05fd057e8a0723d13eef8500009adfaffeb851ba
robert <mann.robertj@gmail.com>
2019-09-29 16:18:10 +0200

    feat/logic to generate board, squares, and bombs added.

---
+++ Minesweeper/src/logic/Board.java
@@ -0,0 +1,67 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Board {
+	private final int width;
+	private final int height;
+	private final double bombDensity;
+	private final List<Square> squares;
+	private boolean lost;
+	private boolean won;
+
+	public Board(int width, int height, double bombDensity) {
+		this.width = width;
+		this.height = height;
+		this.bombDensity = bombDensity;
+		this.squares = new ArrayList<Square>();
+		this.lost = false;
+		this.won = false;
+	}
+
+	public void generateSquares() {
+		Random rand = new Random();
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				Boolean isBomb = rand.nextDouble() < bombDensity;
+				Square newSquare = new Square(i, j, isBomb, this);
+				squares.add(newSquare);
+			}
+		}
+	}
+
+	public boolean isLost() {
+		return lost;
+	}
+
+	public void lose() {
+		lost = true;
+	}
+
+	public boolean isWon() {
+		return won;
+	}
+
+	public void win() {
+		this.won = true;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public double getBombDensity() {
+		return bombDensity;
+	}
+
+	public List<Square> getSquares() {
+		return squares;
+	}
+
+}
---
+++ Minesweeper/src/logic/Square.java
@@ -0,0 +1,48 @@
+package logic;
+
+public class Square {
+	private final int x;
+	private final int y;
+	private final boolean isBomb;
+	private boolean isFlagged;
+	private boolean isRevealed;
+	private final Board board;
+
+	public Square(int x, int y, boolean bomb, Board board) {
+		this.x = x;
+		this.y = y;
+		this.isBomb = bomb;
+		this.isFlagged = false;
+		this.isRevealed = false;
+		this.board = board;
+	}
+
+	public boolean isFlag() {
+		return isFlagged;
+	}
+
+	public boolean isRevealed() {
+		return isRevealed;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public boolean isBomb() {
+		return isBomb;
+	}
+
+	public void reveal() {
+		isRevealed = true;
+	}
+
+	public void toggleFlagged() {
+		isFlagged = !isFlagged;
+	}
+
+}
